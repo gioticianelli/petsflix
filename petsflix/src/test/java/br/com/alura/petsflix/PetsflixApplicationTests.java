8d6621d8e9ccf4018736f5123db983fc160a918f
Giovana Ticianelli <ticianelligiovana@gmail.com>
2026-01-18 18:44:56 -0300

    commit petsflix, ínicio projeto spring

---
+++ petsflix/src/test/java/br/com/alura/petsflix/PetsflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.petsflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
